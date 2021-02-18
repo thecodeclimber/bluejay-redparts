a267f1d64d16d64275404d4eaad09b48323f3046
Moshe Morris <moshem@live.com>
2021-02-18 16:32:52 +0200

    Updated Site styled components to match develop

--- src/styled-components/common/SiteHeader.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-const SiteHeader = styled.header`
-  flex-shrink: ${(props: { mobile?: boolean }) => (props.mobile ? '' : 0)};
-`;
-
-export default SiteHeader;
