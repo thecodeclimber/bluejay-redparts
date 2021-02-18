feb06b1f2d082d6852e9bcba6ed96e2c5945be5e
Moshe Morris <moshem@live.com>
2021-02-18 11:26:45 +0200

    Organized Site Styled Components

--- src/styled-components/common/Site.tsx
+++ src/styled-components/common/Site.tsx
@@ -1,7 +1,24 @@
 import styled from 'styled-components';
 
-const Site = styled.div`
+export const Site = styled.div`
   height: 100%;
 `;
 
-export default Site;
+export const SiteContainer = styled.div`
+min-height: 100%;
+display: flex;
+flex-direction: column;
+overflow: hidden;
+`;
+
+export const SiteBody = styled.div`
+flex-grow: 1;
+`;
+
+export const SiteFooter = styled.footer`
+flex-shrink: 0;
+`;
+
+export const SiteHeader = styled.header`
+flex-shrink: ${(props: { mobile?: boolean }) => (props.mobile ? '' : 0)};
+`;
