9df500d191e5fddc0dd1944612d36b548e3ee65c
Lokesh <lokesh@75way.com>
2021-02-23 16:01:26 +0530

    test branch

--- src/components/header/Indicator.tsx
+++ src/components/header/Indicator.tsx
@@ -1,105 +1,110 @@
 // react
-import React, {
-    PropsWithChildren,
-    useMemo,
-    useRef,
-    useState,
-} from 'react';
-// third-party
-import classNames from 'classnames';
+import React, { PropsWithChildren, useMemo, useRef, useState } from 'react';
 // application
-import AppLink, { IAppLinkHref } from '~/components/shared/AppLink';
+import {
+  IndicatorButton,
+  IndicatorIcon,
+  IndicatorCounter,
+  IndicatorTitle,
+  IndicatorValue,
+  IndicatorContentOpen,
+  IndicatorStyledComponent,
+} from '~/styled-components/header/Indicator';
+import { IAppLinkHref } from '~/components/shared/AppLink';
 import { useGlobalMousedown } from '~/services/hooks';
 
 type Trigger = 'none' | 'click' | 'hover';
 
 export interface IIndicatorController {
-    close: () => void;
+  close: () => void;
 }
 
 interface Props extends PropsWithChildren<{}> {
-    icon: React.ReactNode;
-    href?: IAppLinkHref;
-    label?: React.ReactNode;
-    value?: React.ReactNode;
-    counter?: number;
-    trigger?: Trigger;
-    controllerRef?: React.MutableRefObject<IIndicatorController | null>;
+  icon: React.ReactNode;
+  href?: IAppLinkHref;
+  label?: React.ReactNode;
+  value?: React.ReactNode;
+  counter?: number;
+  trigger?: Trigger;
+  controllerRef?: React.MutableRefObject<IIndicatorController | null>;
 }
 
 function Indicator(props: Props) {
-    const {
-        icon,
-        href,
-        label,
-        value,
-        counter,
-        trigger = 'none',
-        children,
-        controllerRef,
-    } = props;
-    const hasLabel = label !== undefined && label !== null;
-    const hasValue = value !== undefined && value !== null;
-    const buttonType = href !== undefined ? 'link' : 'button';
-    const showCounter = counter !== undefined && counter !== null;
-    const [isOpen, setIsOpen] = useState(false);
-    const rootRef = useRef<HTMLDivElement>(null);
+  const {
+    icon,
+    href,
+    label,
+    value,
+    counter,
+    trigger = 'none',
+    children,
+    controllerRef,
+  } = props;
+  const hasLabel = label !== undefined && label !== null;
+  const hasValue = value !== undefined && value !== null;
+  const buttonType = href !== undefined ? 'link' : 'button';
+  const showCounter = counter !== undefined && counter !== null;
+  const [isOpen, setIsOpen] = useState(false);
+  const rootRef = useRef<HTMLDivElement>(null);
 
-    const controller = useMemo<IIndicatorController>(() => ({
-        close: () => setIsOpen(false),
-    }), [setIsOpen]);
+  const controller = useMemo<IIndicatorController>(
+    () => ({
+      close: () => setIsOpen(false),
+    }),
+    [setIsOpen]
+  );
 
-    if (controllerRef) {
-        controllerRef.current = controller;
+  if (controllerRef) {
+    controllerRef.current = controller;
+  }
+
+  useGlobalMousedown(
+    (event) => {
+      if (
+        rootRef.current &&
+        !rootRef.current.contains(event.target as HTMLElement)
+      ) {
+        setIsOpen(false);
+      }
+    },
+    [setIsOpen, rootRef]
+  );
+
+  const handleButtonClick = (event: React.MouseEvent) => {
+    if (trigger === 'click') {
+      event.preventDefault();
+
+      setIsOpen((prevState) => !prevState);
     }
+  };
 
-    useGlobalMousedown((event) => {
-        if (rootRef.current && !rootRef.current.contains(event.target as HTMLElement)) {
-            setIsOpen(false);
-        }
-    }, [setIsOpen, rootRef]);
+  const buttonContent = (
+    <React.Fragment>
+      <IndicatorIcon>
+        {icon}
+        {showCounter && <IndicatorCounter>{counter}</IndicatorCounter>}
+      </IndicatorIcon>
 
-    const handleButtonClick = (event: React.MouseEvent) => {
-        if (trigger === 'click') {
-            event.preventDefault();
+      {hasLabel && <IndicatorTitle>{label}</IndicatorTitle>}
+      {hasValue && <IndicatorValue>{value}</IndicatorValue>}
+    </React.Fragment>
+  );
 
-            setIsOpen((prevState) => !prevState);
-        }
-    };
-
-    const buttonContent = (
-        <React.Fragment>
-            <span className="indicator__icon">
-                {icon}
-                {showCounter && <span className="indicator__counter">{counter}</span>}
-            </span>
-
-            {hasLabel && <span className="indicator__title">{label}</span>}
-            {hasValue && <span className="indicator__value">{value}</span>}
-        </React.Fragment>
-    );
-
-    const rootClasses = classNames('indicator', `indicator--trigger--${trigger}`, {
-        'indicator--open': isOpen,
-    });
-
-    return (
-        <div className={rootClasses} ref={rootRef}>
-            {buttonType === 'button' && (
-                <button className="indicator__button" type="button" onClick={handleButtonClick}>
-                    {buttonContent}
-                </button>
-            )}
-            {buttonType === 'link' && (
-                <AppLink href={href} className="indicator__button" onClick={handleButtonClick}>
-                    {buttonContent}
-                </AppLink>
-            )}
-            <div className="indicator__content">
-                {children}
-            </div>
-        </div>
-    );
+  return (
+    <IndicatorStyledComponent>
+      {buttonType === 'button' && (
+        <IndicatorButton as="button" type="button" onClick={handleButtonClick}>
+          {buttonContent}
+        </IndicatorButton>
+      )}
+      {buttonType === 'link' && (
+        <IndicatorButton as="a" href={href} onClick={handleButtonClick}>
+          {buttonContent}
+        </IndicatorButton>
+      )}
+      {isOpen && <IndicatorContentOpen>{children}</IndicatorContentOpen>}
+    </IndicatorStyledComponent>
+  );
 }
 
 export default Indicator;
---
+++ src/styled-components/header/Indicator.tsx
@@ -0,0 +1,98 @@
+import styled, { css } from 'styled-components';
+
+const textColor = css`
+  color: #fff;
+`;
+export const IndicatorStyledComponent = styled.div`
+  position: relative;
+`;
+
+export const IndicatorContentOpen = styled.div`
+  pointer-events: auto;
+  z-index: 1;
+  position: absolute;
+  right: 0;
+  transition-delay: 0s, 0s, 0s;
+  opacity: 1;
+  visibility: visible;
+  transform: rotateX(0deg);
+  transition: transform 0.2s, opacity 0.2s;
+`;
+
+export const IndicatorButton = styled.button`
+  ${(props: { href?: any }) =>
+    props.href &&
+    css`
+      ${textColor}
+      display: -moz-box;
+      display: flex;
+      padding: 3px;
+      padding-right: 12px;
+      -moz-box-orient: vertical;
+      -moz-box-direction: normal;
+      flex-direction: column;
+      cursor: pointer;
+      height: 54px;
+      min-width: 54px;
+      border-radius: 2px;
+      border: none;
+      box-sizing: border-box;
+      &:hover {
+        background: rgba(0, 0, 0, 0.15);
+      }
+      &:active {
+        background: rgba(0, 0, 0, 0.15);
+      }
+      &:focus {
+        outline: none;
+      }
+    `}
+`;
+
+export const IndicatorIcon = styled.span`
+  position: absolute;
+  padding: 8px;
+  display: block;
+  width: 48px;
+  height: 48px;
+  fill: #fff;
+`;
+
+export const IndicatorCounter = styled.span`
+  position: absolute;
+  top: 5px;
+  font-size: 10px;
+  line-height: 1;
+  padding: 2px 3px 1px;
+  text-align: center;
+  z-index: 0;
+  right: 4px;
+  color: #262626;
+  font-weight: 500;
+  background-color: #ffdf40;
+  transform: skewX(-13deg);
+  box-sizing: border-box;
+  display: block;
+  border-radius: 2.5px;
+`;
+
+export const IndicatorTitle = styled.span`
+  color: #cdc8e5;
+  margin-left: 50px;
+  padding-top: 3px;
+  margin-bottom: -3px;
+  font-size: 13px;
+  max-width: 120px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: block;
+  white-space: nowrap;
+`;
+
+export const IndicatorValue = styled.span`
+  margin-left: 50px;
+  font-weight: 500;
+  display: block;
+  white-space: nowrap;
+  ${textColor}
+`;
