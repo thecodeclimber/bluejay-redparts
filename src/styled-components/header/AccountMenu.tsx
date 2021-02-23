8b25a4ece3ea6329fba8fa082e7ee3e753cc5db0
Lokesh <lokesh@75way.com>
2021-02-23 18:48:53 +0530

    working on issue #30 create and use account menu styled components

---
+++ src/styled-components/header/AccountMenu.tsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const AccountMenuStyledComponent = styled.div`
+  display: block;
+  width: 280px;
+  background-color: #fff;
+  box-shadow: 0 2px 20px rgb(0 0 0 / 20%);
+  border-radius: 1.5px;
+  color: #262626;
+`;
+
+export const AccountMenuForm = styled.div`
+  padding: 0 30px 32px;
+  display: block;
+  margin-top: 0em;
+`;
+
+export const AccountMenuFormTitle = styled.div`
+  text-align: center;
+  padding: 32px 0 26px;
+  font-weight: 500;
+`;
+
+export const FormGroup = styled.div`
+  margin-bottom: 1rem;
+`;
+
+export const FormControl = styled.div``;
+
+export const InvalidFeedback = styled.div`
+  display: none;
+  width: 100%;
+  margin-top: 0.25rem;
+  font-size: 80%;
+  color: #dc3545;
+`;
+
+export const AccountMenuFormLink = styled.div`
+  font-size: 14px;
+  text-align: center;
+  color: #6c757d;
+`;
