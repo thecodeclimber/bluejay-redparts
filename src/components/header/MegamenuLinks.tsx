9da54358dafb48f12b63665a20b84b1c39afeeb0
lokesh bansal <lokesh@75way.com>
2021-02-26 18:11:06 +0530

    completed issue #37 create and use megamenu links styled components

--- src/components/header/MegamenuLinks.tsx
+++ src/components/header/MegamenuLinks.tsx
@@ -3,56 +3,46 @@ import React from 'react';
 // third-party
 import classNames from 'classnames';
 // application
-import AppLink from '~/components/shared/AppLink';
-import { MegaMenuLinks } from  '~/styled-components/header/MegamenuLinks';
+import {
+  MegaMenuLinks,
+  MegaMenuItemLinksItemLink,
+  MegaMenuLinkItem,
+} from '~/styled-components/header/MegamenuLinks';
 import { ILink, INestedLink } from '~/interfaces/link';
 
 interface Props extends React.HTMLAttributes<HTMLElement> {
-    links: INestedLink[];
-    level?: number;
-    onItemClick?: (item: ILink) => void;
+  links: INestedLink[];
+  level?: number;
+  onItemClick?: (item: ILink) => void;
 }
 
 function MegamenuLinks(props: Props) {
-    const {
-        links,
-        level = 0,
-        onItemClick,
-        className,
-        ...rootProps
-    } = props;
+  const { links, level = 0, onItemClick, className, ...rootProps } = props;
 
-    const rootClasses = classNames('megamenu-links', className, {
-        'megamenu-links--root': level === 0,
-    });
+  return (
+    <MegaMenuLinks  {...rootProps}>
+      {links.map((link, linkIndex) => {
+        const subLinks = link.links || [];
+        const hasSubLinks = subLinks.length > 0;
 
-    return (
-        <MegaMenuLinks className={rootClasses} {...rootProps}>
-            {links.map((link, linkIndex) => {
-                const subLinks = link.links || [];
-                const hasSubLinks = subLinks.length > 0;
-                const linkClasses = classNames('megamenu-links__item', {
-                    'megamenu-links__item--has-submenu': link.links,
-                });
-
-                return (
-                    <li className={linkClasses} key={linkIndex}>
-                        <AppLink
-                            className="megamenu-links__item-link"
-                            href={link.url}
-                            onClick={() => onItemClick && onItemClick(link)}
-                            {...link.customFields?.anchorProps}
-                        >
-                            {link.title}
-                        </AppLink>
-                        {hasSubLinks && (
-                            <MegamenuLinks links={subLinks} level={level + 1} />
-                        )}
-                    </li>
-                );
-            })}
-        </MegaMenuLinks>
-    );
+        return (
+          <MegaMenuLinkItem key={linkIndex}>
+            <MegaMenuItemLinksItemLink
+              hasSubLinks={hasSubLinks}
+              href={link.url}
+              onClick={() => onItemClick && onItemClick(link)}
+              {...link.customFields?.anchorProps}
+            >
+              {link.title}
+            </MegaMenuItemLinksItemLink>
+            {hasSubLinks && (
+              <MegamenuLinks links={subLinks} level={level + 1} />
+            )}
+          </MegaMenuLinkItem>
+        );
+      })}
+    </MegaMenuLinks>
+  );
 }
 
 export default React.memo(MegamenuLinks);
--- src/styled-components/header/MegamenuLinks.tsx
+++ src/styled-components/header/MegamenuLinks.tsx
@@ -1,66 +1,48 @@
 import styled, { css } from 'styled-components';
 
-export const MegaMenuLinks = styled.ul` 
-list-style: none;
-padding: 0;
-margin: 0;
+export const MegaMenuLinks = styled.ul`
+  list-style: none;
+  padding: 0;
+  margin: 0;
 `;
 
-// export const MegaMenuItemLinksItemLink = styled.a`
-//    ${(props :{ href?: any }) =>
-//          !props.href ?
-//          css `
-//          font-weight: 400;
-//          font-size: 14px;
-//          color: inherit;
-//          text-decoration: none;
-//          background-color: transparent;
-//         cursor: pointer
-//         list-style: none;
-//         margin-top: 5px;
-//         &:hover {
-//         color: #1961bb;
-//        }
-//          `:
-//         css `
-//         font-size: 15px;
-//         color: #6c757d;
-//         color: inherit;
-//         transition: color .15s;
-//         font-weight: 500;
-//         text-decoration: none;
-//         border-bottom: 1px solid #ebebeb;
-//         padding-bottom: 12px;
-//         margin-bottom: 5px;
-//         background-color: transparent;
-//         cursor: pointer;
-
-//         list-style: none;
-//        &:hover {
-//         color: #1961bb;
-//        }
-//       @media (min-width: 1200px) {
-//         font-size: 15px;
-//       }
-//         `}
-
-// `;
+export const MegaMenuLinkItem = styled.li`
+  margin-top: 4px;
+`;
 
 export const MegaMenuItemLinksItemLink = styled.a`
-  ${(props: { href?: any }) =>
-    props.href &&
-    css`
-      color: inherit;
-      transition: color 0.15s;
-      font-weight: 500;
-      font-size: 10px;
-      padding-top: 12px;
-      margin-top: 5px;
-      text-decoration: none;
-      
-      background-color: transparent;
-      @media (min-width: 1200px) and (max-width: 1399.98px) {
-        font-size: 15px;
-      }
-    `}
+  ${(props: { hasSubLinks?: any }) =>
+    !props.hasSubLinks
+      ? css`
+         font-weight: 400;
+         font-size: 14px;
+         color: #6c757d;
+         text-decoration: none;
+         background-color: transparent;
+         line-height: 18px;
+         cursor: pointer
+         list-style: none;
+        &:hover {
+        color: #1961bb;
+       }
+         `
+      : css`
+          font-size: 15px;
+          color: #6c757d;
+          color: inherit;
+          transition: color 0.15s;
+          font-weight: 500;
+          text-decoration: none;
+          border-bottom: 1px solid #ebebeb;
+          background-color: transparent;
+          padding-bottom: 5px;
+          cursor: pointer;
+          list-style: none;
+          &:hover {
+            color: #1961bb;
+          }
+          @media (min-width: 1200px) {
+            font-size: 15px;
+          }
+        `}
 `;
