6d4c802b65ff9c22777a9524e088c2b804db0464
Lokesh <lokesh@75way.com>
2021-03-09 18:38:12 +0530

    working on issue #86 create and use widget categories list styled components

--- src/components/widgets/WidgetCategoriesList.tsx
+++ src/components/widgets/WidgetCategoriesList.tsx
@@ -1,114 +1,104 @@
-// react
-import React ,{useState} from 'react';
+import React, { useState } from 'react';
 // third-party
 import classNames from 'classnames';
 // application
+import {
+  WidgetCategoriesListRootItem,
+  WidgetCategoriesListRootLink,
+  WidgetCategoriesListShowMoreButton,
+  WidgetCategoriesListChildItem,
+  WidgetCategoriesListChild,
+} from '~/styled-components/widget/WidgetCategoriesList';
 import AppLink from '~/components/shared/AppLink';
 import Collapse, { ICollapseRenderFnData } from '~/components/shared/Collapse';
 import url from '~/services/url';
 import { ArrowDown9x6Svg } from '~/svg';
 import { ICategory } from '~/interfaces/category';
-import {
-    WidgetWidgetCategoriesList ,
-    WidgetCategoriesListBody ,
-    WidgetCategoriesListRoot ,
-    WidgetCategoriesListRootItem ,
-    WidgetCategoriesListRootLink ,
-    WidgetCategoriesListChild ,
-    WidgetCategoriesListChildItem ,
-    WidgetCategoriesListShowMore ,
-    WidgetCategoriesListShowMoreExpanText ,
-    WidgetCategoriesListShowMoreCollapseText ,
-    WidgetCategoriesListShowMoreArrow 
-}
- from '~/styled-components/widget/WidgetCategoriesList';
+
 type RenderFnData = ICollapseRenderFnData<HTMLLIElement, HTMLUListElement>;
 type RenderFn = (data: RenderFnData, category: ICategory) => React.ReactNode;
 
 interface Props {
-    categories: ICategory[];
+  categories: ICategory[];
 }
 
 function WidgetCategoriesList(props: Props) {
-    const { categories } = props;
-    const [isOpen , setIsOpen] = useState(false);
-       console.log(isOpen);
-    const handleOpen = () => {
-        setIsOpen(!isOpen);
-    }
-    const renderCategory: RenderFn = ({ toggle, setItemRef, setContentRef }, category: ICategory) => {
-        const subs: ICategory[] = category.children || [];
-        return (
-            <WidgetCategoriesListRootItem
-                length ={category.children?.length}
-                ref={setItemRef}
-            >
-                <WidgetCategoriesListRootLink
-                    href={url.category(category)}
-                >
-                    {category.name}
-                </WidgetCategoriesListRootLink>
+  const { categories } = props;
+  const [isOpen, setIsOpen] = useState(false);
 
-                {subs.length > 0 && (
-                    < WidgetCategoriesListChild isOpen={isOpen} className="widget-categories-list__child">
-                        {subs.slice(0, subs.length > 6 ? 5 : 6).map((sub, subIdx) => (
-                            < WidgetCategoriesListChildItem key={subIdx} >
-                                <AppLink
-                                    href={url.category(sub)}
-                                >
-                                    {sub.name}
-                                </AppLink>
-                            </ WidgetCategoriesListChildItem>
-                        ))}
-                    </ WidgetCategoriesListChild>
-                )}
+  const handleToggle = () => {
+    setIsOpen((prevCheck) => !prevCheck);
+  };
 
-                {subs.length > 6 && (
-                    <React.Fragment>
-                        < WidgetCategoriesListChild className="widget-categories-list__child" ref={setContentRef}>
-                            {subs.slice(5).map((sub, subIdx) => (
-                                <WidgetCategoriesListChildItem
-                                    key={subIdx}
-                                   
-                                >
-                                    <AppLink
-                                        href={url.category(sub)}
-                                    >
-                                        {sub.name}
-                                    </AppLink>
-                                </WidgetCategoriesListChildItem>
-                            ))}
-                        </WidgetCategoriesListChild>
-                        <WidgetCategoriesListShowMore
-                           isOpen = {isOpen}
-                            type="button"
-                            onClick={() => {toggle() , handleOpen()}}
-                        >
-                            <WidgetCategoriesListShowMoreExpanText >Show More</WidgetCategoriesListShowMoreExpanText>
-                            <WidgetCategoriesListShowMoreCollapseText >Show Less</WidgetCategoriesListShowMoreCollapseText>
-                            <WidgetCategoriesListShowMoreArrow ><ArrowDown9x6Svg /></WidgetCategoriesListShowMoreArrow>
-                        </WidgetCategoriesListShowMore>
-                    </React.Fragment>
-                )}
-            </WidgetCategoriesListRootItem>
-        );
-    };
+  const renderCategory: RenderFn = (
+    { setItemRef, setContentRef },
+    category: ICategory
+  ) => {
+    const subs: ICategory[] = category.children || [];
 
     return (
-        <WidgetWidgetCategoriesList>
-            <WidgetCategoriesListBody >
-                <WidgetCategoriesListRoot >
-                    {categories.map((category, categoryIdx) => (
-                        <Collapse<HTMLLIElement, HTMLUListElement>
-                            key={categoryIdx}
-                            toggleClass="widget-categories-list--open"
-                            render={(args) => renderCategory(args, category)}
-                        />
-                    ))}
-                </WidgetCategoriesListRoot>
-            </WidgetCategoriesListBody>
-        </WidgetWidgetCategoriesList>
+      <WidgetCategoriesListRootItem
+        ref={setItemRef}
+        hasChild={category.children?.length}
+      >
+        <WidgetCategoriesListRootLink href={url.category(category)}>
+          {category.name}
+        </WidgetCategoriesListRootLink>
+
+        {subs.length > 0 && (
+          <WidgetCategoriesListChild isOpen={true}>
+            {subs.slice(0, subs.length > 6 ? 5 : 6).map((sub, subIdx) => (
+              <WidgetCategoriesListChildItem key={subIdx}>
+                <AppLink href={url.category(sub)}>{sub.name}</AppLink>
+              </WidgetCategoriesListChildItem>
+            ))}
+          </WidgetCategoriesListChild>
+        )}
+
+        {subs.length > 6 && (
+          <React.Fragment>
+            <WidgetCategoriesListChild ref={setContentRef} isOpen={isOpen}>
+              {subs.slice(5).map((sub, subIdx) => (
+                <WidgetCategoriesListChildItem key={subIdx}>
+                  <AppLink href={url.category(sub)}>{sub.name}</AppLink>
+                </WidgetCategoriesListChildItem>
+              ))}
+            </WidgetCategoriesListChild>
+            <WidgetCategoriesListShowMoreButton
+              type="button"
+              onClick={handleToggle}
+            >
+              <span className="widget-categories-list__show-more-expand-text">
+                Show More
+              </span>
+              <span className="widget-categories-list__show-more-collapse-text">
+                Show Less
+              </span>
+              <span className="widget-categories-list__show-more-arrow">
+                <ArrowDown9x6Svg />
+              </span>
+            </WidgetCategoriesListShowMoreButton>
+          </React.Fragment>
+        )}
+      </WidgetCategoriesListRootItem>
     );
+  };
+
+  return (
+    <div className="card widget widget-categories-list">
+      <div className="widget-categories-list__body">
+        <ul className="widget-categories-list__root">
+          {categories.map((category, categoryIdx) => (
+            <Collapse<HTMLLIElement, HTMLUListElement>
+              key={categoryIdx}
+              toggleClass="widget-categories-list--open"
+              render={(args) => renderCategory(args, category)}
+            />
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default WidgetCategoriesList;
--- src/styled-components/widget/WidgetCategoriesList.tsx
+++ src/styled-components/widget/WidgetCategoriesList.tsx
@@ -1,120 +1,60 @@
 import styled, { css } from 'styled-components';
 import AppLink from '~/components/shared/AppLink';
 
-export const WidgetWidgetCategoriesList = styled.div`
-  background-color: #fff;
-  box-shadow: 0 1px 3px rgb(0 0 0 / 9%);
-  border: none;
-  border-radius: 0;
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  min-width: 0;
-  background-clip: border-box;
-  margin-bottom: 20px;
-`;
-
-export const WidgetCategoriesListBody = styled.div`
-  padding: 1.5rem;
-  a {
-    color: inherit;
-    transition: color 0.1s;
-  }
-  a:hover {
-    color: #007bff;
-  }
-`;
-
-export const WidgetCategoriesListRoot = styled.ul`
-  list-style: none;
-  margin: 0;
-  padding: 0;
-`;
-
 export const WidgetCategoriesListRootItem = styled.li`
-  margin-top: ${(props: { length?: any }) =>
-    props.length ? '1.375rem' : '.375rem'};
-  margin-bottom: 0.375rem;
   line-height: 20px;
+  ${(props: { hasChild?: any }) =>
+    props.hasChild &&
+    css`
+      margin-top: 1.375rem;
+    `}
 `;
 
 export const WidgetCategoriesListRootLink = styled(AppLink)`
+  color: inherit;
+  transition: color 0.1s;
   font-weight: 500;
   font-size: 17px;
 `;
 
-export const WidgetCategoriesListChild = styled.ul`
-  transition: height 0.2s ease-in-out, opacity 0.2s ease-in-out;
-  list-style: none;
-  padding: 0;
-  font-size: 15px;
-  margin: 10px 0 0;
-  ${(props: { isOpen?: boolean }) =>
-    props.isOpen ?
-    css`
-      height: auto;
-      opacity: 1;
-    `:
-    css`
-    font-size: 15px;
-    `}
-`;
 export const WidgetCategoriesListChildItem = styled.li`
   padding: 3px 0;
 `;
 
-export const WidgetCategoriesListShowMoreExpanText = styled.span``;
-
-export const WidgetCategoriesListShowMoreCollapseText = styled.span`
-  display: none;
+export const WidgetCategoriesListChild = styled.ul`
+  margin-top: 0;
+  overflow: hidden;
+  height: 0;
+  opacity: 0;
+  transition: height 0.2s ease-in-out, opacity 0.2s ease-in-out;
+  ${(props: { isOpen?: any }) =>
+    props.isOpen &&
+    css`
+      height: 100%;
+      opacity: 1;
+    `}
 `;
 
 export const WidgetCategoriesListShowMoreArrow = styled.span`
-  fill: currentColor;
-  position: absolute;
-  top: 6px;
-  transition: transform 0.2s;
-  opacity: 0.78;
-  left: 0;
-  svg {
-    display: block;
-  }
+  transform: rotate(0deg);
+  ${(props: { isOpen?: any }) =>
+    props.isOpen &&
+    css`
+      transform: rotate(180deg);
+    `}
 `;
 
-export const WidgetCategoriesListShowMore = styled.button`
+
+
+export const WidgetCategoriesListShowMoreButton = styled.button`
+  direction: ltr;
+  padding-left: 16px;
   position: relative;
   display: inline;
   border: none;
   background: transparent;
-  padding: 0;
   color: #999;
   font-size: 14px;
   font-family: inherit;
   margin-top: 4px;
-  padding-left: 16px;
-  ${(props: { isOpen?: boolean }) =>
-    props.isOpen
-      ? css`
-          ${WidgetCategoriesListShowMoreExpanText} {
-            display: none;
-          }
-          ${WidgetCategoriesListShowMoreCollapseText} {
-            display: inline;
-          }
-        `
-      : css`
-          ${WidgetCategoriesListShowMoreExpanText} {
-            display: inline;
-          }
-
-          ${WidgetCategoriesListShowMoreArrow} {
-            transform: rotate(180deg);
-          }
-        `}
-  &:hover {
-    text-decoration: underline;
-  }
-  &:focus {
-    outline: none;
-  }
 `;
