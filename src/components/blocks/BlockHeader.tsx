5486adc51659b62513ba701f46dd2ef30f756778
Lokesh <lokesh@75way.com>
2021-03-11 10:59:42 +0530

    temp

--- src/components/blocks/BlockHeader.tsx
+++ src/components/blocks/BlockHeader.tsx
@@ -3,65 +3,71 @@ import React, { useEffect, useRef } from 'react';
 // third-party
 import classNames from 'classnames';
 // application
-import Breadcrumb from '~/components/shared/Breadcrumb';
+import {
+  BlockHeaderBody,
+  BlockHeaderTitle,
+  BlockHeaderBreadcrumb,
+  BlockHeaderStyledComponent
+} from '~/styled-components/block/BlockHeader';
 import { ILink } from '~/interfaces/link';
 import { useMedia } from '~/store/hooks';
 
 interface Props {
-    pageTitle?: React.ReactNode;
-    breadcrumb?: ILink[];
-    afterHeader?: boolean;
+  pageTitle?: React.ReactNode;
+  breadcrumb?: ILink[];
+  afterHeader?: boolean;
 }
 
 function BlockHeader(props: Props) {
-    const { pageTitle, breadcrumb = [], afterHeader = true } = props;
-    const hasPageTitle = !!pageTitle;
-    const hasBreadcrumb = breadcrumb.length > 0;
-    const elementRef = useRef<HTMLDivElement | null>(null);
-    const titleElementRef = useRef<HTMLHeadingElement | null>(null);
-    const titleVisible = useMedia('(min-width: 1200px)');
+  const { pageTitle, breadcrumb = [], afterHeader = true } = props;
+  const hasPageTitle = !!pageTitle;
+  const hasBreadcrumb = breadcrumb.length > 0;
+  const elementRef = useRef<HTMLDivElement | null>(null);
+  const titleElementRef = useRef<HTMLHeadingElement | null>(null);
+  const titleVisible = useMedia('(min-width: 1200px)');
 
-    const calcTitleWidth = () => {
-        // So that breadcrumbs correctly flow around the page title, we need to know its width.
-        // This code simply conveys the width of the page title in CSS.
+  const calcTitleWidth = () => {
+    // So that breadcrumbs correctly flow around the page title, we need to know its width.
+    // This code simply conveys the width of the page title in CSS.
 
-        if (!elementRef.current || !titleElementRef.current) {
-            return;
-        }
+    if (!elementRef.current || !titleElementRef.current) {
+      return;
+    }
 
-        const { width } = titleElementRef.current.getBoundingClientRect();
+    const { width } = titleElementRef.current.getBoundingClientRect();
 
-        elementRef.current.style.setProperty('--block-header-title-width', `${width}px`);
-    };
-
-    useEffect(() => {
-        if (titleVisible) {
-            calcTitleWidth();
-        }
-    }, [pageTitle, titleVisible]);
-
-    const rootClasses = classNames('block-header', {
-        'block-header--has-title': hasPageTitle,
-        'block-header--has-breadcrumb': hasBreadcrumb,
-    });
-
-    return (
-        <div className={rootClasses} ref={elementRef}>
-            <div className="container">
-                <div className="block-header__body">
-                    {hasBreadcrumb && (
-                        <Breadcrumb
-                            className="block-header__breadcrumb"
-                            items={breadcrumb}
-                            withPageTitle={hasPageTitle}
-                            afterHeader={afterHeader}
-                        />
-                    )}
-                    {hasPageTitle && <h1 className="block-header__title" ref={titleElementRef}>{pageTitle}</h1>}
-                </div>
-            </div>
-        </div>
+    elementRef.current.style.setProperty(
+      '--block-header-title-width',
+      `${width}px`
     );
+  };
+
+  useEffect(() => {
+    if (titleVisible) {
+      calcTitleWidth();
+    }
+  }, [pageTitle, titleVisible]);
+
+  return (
+    <BlockHeaderStyledComponent ref={elementRef}>
+      <div className="container">
+        <BlockHeaderBody>
+          {hasBreadcrumb && (
+            <BlockHeaderBreadcrumb
+              items={breadcrumb}
+              withPageTitle={hasPageTitle}
+              afterHeader={afterHeader}
+            />
+          )}
+          {hasPageTitle && (
+            <BlockHeaderTitle ref={titleElementRef}>
+              {pageTitle}
+            </BlockHeaderTitle>
+          )}
+        </BlockHeaderBody>
+      </div>
+    </BlockHeaderStyledComponent>
+  );
 }
 
 export default BlockHeader;
