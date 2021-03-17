31aec211eb83134f9d9a04b9fda1310730ab89ba
lokesh bansal <lokesh@75way.com>
2021-03-17 18:23:16 +0530

    closes  #146 create and use product tabs styled components

--- src/theme/Theme.tsx
+++ src/theme/Theme.tsx
@@ -58,6 +58,7 @@ const theme: DefaultTheme = {
     boxshadowcolor: '#00000017',
     bordercolor: '#ebebeb',
     subtitilecolor: '#6c757d',
+    tabbgcolor:'#f7f7f7',
   },
   headers: {
     h1: {
--- src/theme/Theme.type.tsx
+++ src/theme/Theme.type.tsx
@@ -57,6 +57,7 @@ declare module 'styled-components' {
       boxshadowcolor: string;
       bordercolor: string;
       subtitilecolor: string;
+      tabbgcolor:string;
     };
     headers: {
       h1: {
