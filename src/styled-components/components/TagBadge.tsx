2ddcc345a97937338ea1771d210e52550ea4d8cc
Lokesh <lokesh@75way.com>
2021-04-06 18:01:58 +0530

    fixed issue in addresses page

--- src/styled-components/components/TagBadge.tsx
+++ src/styled-components/components/TagBadge.tsx
@@ -4,6 +4,23 @@ const TagBadgeTheme = css`
   color: ${(props) => `${props.theme.colors.white}`};
 `;
 
+export const AddressTagBadge = css`
+  display: inline-block;
+  text-transform: uppercase;
+  font-size: 11px;
+  font-weight: ${(props) => `${props.theme.fontWeight.bolder}`};
+  line-height: 1;
+  z-index: 0;
+  color: ${(props) => `${props.theme.colors.white}`};
+  background: ${(props) => `${props.theme.colors.primary}`};
+  height: 18px;
+  padding: 4px 14px 0;
+  -webkit-transform: skewX(-13deg);
+  -ms-transform: skewX(-13deg);
+  transform: skewX(-13deg);
+  border-radius: 2px;
+`;
+
 export const TagBadge = styled.div`
   display: inline-block;
   position: relative;
