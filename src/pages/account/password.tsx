7021aacb227faf440891a4d246c7264323d9f3e7
Lokesh <lokesh@75way.com>
2021-04-19 18:10:24 +0530

    closes Issue #236 create and use password styled components.

--- src/pages/account/password.tsx
+++ src/pages/account/password.tsx
@@ -1,6 +1,12 @@
 // react
 import React, { useState } from 'react';
 // third-party
+import {
+  CardStyledComponent,
+  CardHeader,
+  CardDivider,
+  CardBodyPadding2,
+} from '~/styled-components/components/Card';
 import classNames from 'classnames';
 import { FormattedMessage, useIntl } from 'react-intl';
 import { toast } from 'react-toastify';
@@ -12,135 +18,150 @@ import { accountApi } from '~/api';
 import { useAsyncAction } from '~/store/hooks';
 
 interface IForm {
-    currentPassword: string;
-    newPassword: string;
-    confirmPassword: string;
+  currentPassword: string;
+  newPassword: string;
+  confirmPassword: string;
 }
 
 function Page() {
-    const intl = useIntl();
-    const [serverError, setServerError] = useState<string | null>(null);
-    const {
-        register,
-        errors,
-        watch,
-        handleSubmit,
-    } = useForm<IForm>();
+  const intl = useIntl();
+  const [serverError, setServerError] = useState<string | null>(null);
+  const { register, errors, watch, handleSubmit } = useForm<IForm>();
 
-    const [submit, submitInProgress] = useAsyncAction((data: IForm) => {
-        setServerError(null);
+  const [submit, submitInProgress] = useAsyncAction(
+    (data: IForm) => {
+      setServerError(null);
 
-        return accountApi.changePassword(data.currentPassword, data.newPassword).then(
-            () => {
-                toast.success(intl.formatMessage({ id: 'TEXT_TOAST_PASSWORD_CHANGED' }));
-            },
-            (error: Error) => {
-                setServerError(`ERROR_API_${error.message}`);
-            },
+      return accountApi
+        .changePassword(data.currentPassword, data.newPassword)
+        .then(
+          () => {
+            toast.success(
+              intl.formatMessage({ id: 'TEXT_TOAST_PASSWORD_CHANGED' })
+            );
+          },
+          (error: Error) => {
+            setServerError(`ERROR_API_${error.message}`);
+          }
         );
-    }, [intl]);
+    },
+    [intl]
+  );
 
-    return (
-        <div className="card">
-            <PageTitle>{intl.formatMessage({ id: 'HEADER_CHANGE_PASSWORD' })}</PageTitle>
+  return (
+    <CardStyledComponent>
+      <PageTitle>
+        {intl.formatMessage({ id: 'HEADER_CHANGE_PASSWORD' })}
+      </PageTitle>
 
-            <div className="card-header">
-                <h5><FormattedMessage id="HEADER_CHANGE_PASSWORD" /></h5>
+      <CardHeader>
+        <h5>
+          <FormattedMessage id="HEADER_CHANGE_PASSWORD" />
+        </h5>
+      </CardHeader>
+      <CardDivider />
+      <CardBodyPadding2 className="card-body">
+        <div className="row no-gutters">
+          <form
+            onSubmit={handleSubmit(submit)}
+            className="col-12 col-lg-7 col-xl-6"
+          >
+            {serverError && (
+              <div className="alert alert-sm alert-danger">
+                <FormattedMessage id={serverError} />
+              </div>
+            )}
+
+            <div className="form-group">
+              <label htmlFor="password-current">
+                <FormattedMessage id="INPUT_PASSWORD_CURRENT_LABEL" />
+              </label>
+              <input
+                type="password"
+                id="password-current"
+                name="currentPassword"
+                className={classNames('form-control', {
+                  'is-invalid': errors.currentPassword,
+                })}
+                placeholder={intl.formatMessage({
+                  id: 'INPUT_PASSWORD_CURRENT_PLACEHOLDER',
+                })}
+                ref={register({ required: true })}
+              />
+              <div className="invalid-feedback">
+                {errors.currentPassword?.type === 'required' && (
+                  <FormattedMessage id="ERROR_FORM_REQUIRED" />
+                )}
+              </div>
             </div>
-            <div className="card-divider" />
-            <div className="card-body card-body--padding--2">
-                <div className="row no-gutters">
-                    <form onSubmit={handleSubmit(submit)} className="col-12 col-lg-7 col-xl-6">
-                        {serverError && (
-                            <div className="alert alert-sm alert-danger">
-                                <FormattedMessage id={serverError} />
-                            </div>
-                        )}
-
-                        <div className="form-group">
-                            <label htmlFor="password-current">
-                                <FormattedMessage id="INPUT_PASSWORD_CURRENT_LABEL" />
-                            </label>
-                            <input
-                                type="password"
-                                id="password-current"
-                                name="currentPassword"
-                                className={classNames('form-control', {
-                                    'is-invalid': errors.currentPassword,
-                                })}
-                                placeholder={intl.formatMessage({ id: 'INPUT_PASSWORD_CURRENT_PLACEHOLDER' })}
-                                ref={register({ required: true })}
-                            />
-                            <div className="invalid-feedback">
-                                {errors.currentPassword?.type === 'required' && (
-                                    <FormattedMessage id="ERROR_FORM_REQUIRED" />
-                                )}
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password-new">
-                                <FormattedMessage id="INPUT_PASSWORD_NEW_LABEL" />
-                            </label>
-                            <input
-                                type="password"
-                                id="password-new"
-                                name="newPassword"
-                                className={classNames('form-control', {
-                                    'is-invalid': errors.newPassword,
-                                })}
-                                placeholder={intl.formatMessage({ id: 'INPUT_PASSWORD_NEW_PLACEHOLDER' })}
-                                ref={register({ required: true })}
-                            />
-                            <div className="invalid-feedback">
-                                {errors.newPassword?.type === 'required' && (
-                                    <FormattedMessage id="ERROR_FORM_REQUIRED" />
-                                )}
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="password-confirm">
-                                <FormattedMessage id="INPUT_PASSWORD_REPEAT_LABEL" />
-                            </label>
-                            <input
-                                type="password"
-                                id="password-confirm"
-                                name="confirmPassword"
-                                className={classNames('form-control', {
-                                    'is-invalid': errors.confirmPassword,
-                                })}
-                                placeholder={intl.formatMessage({ id: 'INPUT_PASSWORD_REPEAT_PLACEHOLDER' })}
-                                ref={register({
-                                    required: true,
-                                    validate: {
-                                        match: (value) => value === watch('newPassword'),
-                                    },
-                                })}
-                            />
-                            <div className="invalid-feedback">
-                                {errors.confirmPassword?.type === 'required' && (
-                                    <FormattedMessage id="ERROR_FORM_REQUIRED" />
-                                )}
-                                {errors.confirmPassword?.type === 'match' && (
-                                    <FormattedMessage id="ERROR_FORM_PASSWORD_DOES_NOT_MATCH" />
-                                )}
-                            </div>
-                        </div>
-
-                        <div className="form-group mb-0">
-                            <button
-                                type="submit"
-                                className={classNames('btn', 'btn-primary', 'mt-3', {
-                                    'btn-loading': submitInProgress,
-                                })}
-                            >
-                                <FormattedMessage id="BUTTON_CHANGE" />
-                            </button>
-                        </div>
-                    </form>
-                </div>
+            <div className="form-group">
+              <label htmlFor="password-new">
+                <FormattedMessage id="INPUT_PASSWORD_NEW_LABEL" />
+              </label>
+              <input
+                type="password"
+                id="password-new"
+                name="newPassword"
+                className={classNames('form-control', {
+                  'is-invalid': errors.newPassword,
+                })}
+                placeholder={intl.formatMessage({
+                  id: 'INPUT_PASSWORD_NEW_PLACEHOLDER',
+                })}
+                ref={register({ required: true })}
+              />
+              <div className="invalid-feedback">
+                {errors.newPassword?.type === 'required' && (
+                  <FormattedMessage id="ERROR_FORM_REQUIRED" />
+                )}
+              </div>
             </div>
+            <div className="form-group">
+              <label htmlFor="password-confirm">
+                <FormattedMessage id="INPUT_PASSWORD_REPEAT_LABEL" />
+              </label>
+              <input
+                type="password"
+                id="password-confirm"
+                name="confirmPassword"
+                className={classNames('form-control', {
+                  'is-invalid': errors.confirmPassword,
+                })}
+                placeholder={intl.formatMessage({
+                  id: 'INPUT_PASSWORD_REPEAT_PLACEHOLDER',
+                })}
+                ref={register({
+                  required: true,
+                  validate: {
+                    match: (value) => value === watch('newPassword'),
+                  },
+                })}
+              />
+              <div className="invalid-feedback">
+                {errors.confirmPassword?.type === 'required' && (
+                  <FormattedMessage id="ERROR_FORM_REQUIRED" />
+                )}
+                {errors.confirmPassword?.type === 'match' && (
+                  <FormattedMessage id="ERROR_FORM_PASSWORD_DOES_NOT_MATCH" />
+                )}
+              </div>
+            </div>
+
+            <div className="form-group mb-0">
+              <button
+                type="submit"
+                className={classNames('btn', 'btn-primary', 'mt-3', {
+                  'btn-loading': submitInProgress,
+                })}
+              >
+                <FormattedMessage id="BUTTON_CHANGE" />
+              </button>
+            </div>
+          </form>
         </div>
-    );
+      </CardBodyPadding2>
+    </CardStyledComponent>
+  );
 }
 
 Page.Layout = AccountLayout;
--- src/styled-components/components/Card.tsx
+++ src/styled-components/components/Card.tsx
@@ -1,5 +1,33 @@
 import styled, { css } from 'styled-components';
 
+export const CardStyledComponent = styled.div`
+  background-color: #fff;
+  box-shadow: 0 1px 3px rgb(0 0 0 / 9%);
+  border: none;
+  border-radius: 0;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  min-width: 0;
+  word-wrap: break-word;
+  background-clip: border-box;
+`;
+
+export const CardHeader = styled.div`
+  background: transparent;
+  padding: 1.25rem 2rem;
+  border: none;
+  margin-bottom: 0;
+  &:first-child {
+    border-radius: calc(0.25rem - 1px) calc(0.25rem - 1px) 0 0;
+  }
+`;
+
+export const CardDivider = styled.div`
+  height: 1px;
+  background: #ebebeb;
+`;
+
 const CardTitleLg = css`
   font-size: 28px;
 `;
