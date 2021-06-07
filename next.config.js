32c7e33524995d95d77cc0cc601af17b65f5a90f
Lokesh <lokesh@75way.com>
2021-06-07 20:25:04 +0530

    updated base url for images

--- next.config.js
+++ next.config.js
@@ -3,9 +3,18 @@ const defaultLocale = 'en';
 
 // noinspection JSUnusedGlobalSymbols
 module.exports = {
+  // env: {
+  //   basePath: process.env.BASE_PATH || '',
+  // },
+  // basePath: process.env.BASE_PATH || '',
   env: {
     DATABASE_URL:
       'mongodb+srv://lokeshBansal:lokesh@123@bluejay-sandbox.3wrb7.mongodb.net/bluejay?retryWrites=true&w=majority',
+  },
+  images: {
+    domains: [
+      'https://bluejay-redparts-4zenebxpr-bluejay-fasteners.vercel.app',
+    ],
   },
   async rewrites() {
     return [
